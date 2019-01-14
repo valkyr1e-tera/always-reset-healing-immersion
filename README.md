13e1fd409834253398109f0c0297d9721acfcf0d
valkyr1e <nkpoidjp+tera@gmail.com>
2019-01-14 12:14:35 +0900

    bugfix

---
+++ README.md
@@ -0,0 +1,6 @@
+always-reset-healing-immersion
+===
+Try healing immersion until initialization.
+
+## Note
+- If you are using a skill emulation mod like `skill-prediction`, you have to turn off emulation of healing-immersion now.
--- index.js
+++ index.js
@@ -5,19 +5,21 @@ module.exports = function AlwaysResetHealingImmersion(mod) {
       reset = true
   })
 
-  mod.hook('S_ACTION_STAGE', 9, { order: Number.NEGATIVE_INFINITY }, event => {
+  mod.hook('C_START_SKILL', 7, { order: Number.NEGATIVE_INFINITY, filter: { fake: null } }, event => {
     if (mod.game.me.class !== 'priest')
       return
 
     if (event.skill.id === 370200) {
-      if (reset) {
-        reset = false
-      } else {
-        mod.hookOnce('S_ACTION_END', 5, () => {
-          mod.send('C_START_SKILL', 7, event)
-        })
-        mod.send('C_CANCEL_SKILL', 3, { skill: event.skill, type: 1 })
-      }
+      mod.hookOnce('S_ACTION_STAGE', 9, () => {
+        if (reset) {
+          reset = false
+        } else {
+          mod.hookOnce('S_ACTION_END', 5, () => {
+            mod.send('C_START_SKILL', 7, event)
+          })
+          mod.send('C_CANCEL_SKILL', 3, { skill: event.skill, type: 1 })
+        }
+      })
     }
   })
 }
